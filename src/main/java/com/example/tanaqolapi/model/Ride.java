763fac662c1fb72feb68f56d1c3382389f2554b5
NabilChabab <nabil.chababnabil@gmail.com>
2025-02-09 13:31:59 +0100

    vehicle for a driver

---
+++ src/main/java/com/example/tanaqolapi/model/Ride.java
@@ -0,0 +1,44 @@
+package com.example.tanaqolapi.model;
+
+
+import com.example.tanaqolapi.model.enums.RideStatus;
+import com.example.tanaqolapi.model.enums.RideType;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Ride {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(nullable = false)
+    private UUID id;
+
+    @Enumerated(EnumType.STRING)
+    private RideType rideType;
+
+    private String pickupLocation;
+    private String destination;
+    private LocalDateTime pickupTime;
+    private double price;
+
+    @Enumerated(EnumType.STRING)
+    private RideStatus rideStatus;
+
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    private AppUser customer;
+
+    @ManyToOne
+    @JoinColumn(name = "driver_id", nullable = false)
+    private AppUser driver;
+
+}
