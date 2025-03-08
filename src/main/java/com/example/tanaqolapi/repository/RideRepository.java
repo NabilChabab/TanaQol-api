2106b1f37051a3f4881c5c1d0b00ec68c7702d14
NabilChabab <nabil.chababnabil@gmail.com>
2025-03-08 21:24:50 +0000

    integrate google maps API

--- src/main/java/com/example/tanaqolapi/repository/RideRepository.java
+++ src/main/java/com/example/tanaqolapi/repository/RideRepository.java
@@ -1,10 +1,16 @@
 package com.example.tanaqolapi.repository;
 
 import com.example.tanaqolapi.model.Ride;
+import com.example.tanaqolapi.model.enums.RideStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface RideRepository extends JpaRepository<Ride, UUID> {
+
+    List<Ride> findByCustomerId(UUID customerId);
+    List<Ride> findByDriverId(UUID driverId);
+    List<Ride> findByRideStatus(RideStatus status);
 
 }
