4de828c4428655e57b8980ab508ea9c2e69a15ff
NabilChabab <nabil.chababnabil@gmail.com>
2025-03-26 18:00:42 +0000

    integrate websockets

--- src/main/java/com/example/tanaqolapi/notifications/NotificationService.java
+++ src/main/java/com/example/tanaqolapi/notifications/NotificationService.java
@@ -1,22 +1,36 @@
 package com.example.tanaqolapi.notifications;
 
+import com.example.tanaqolapi.model.Ride;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @Service
 public class NotificationService {
 
     @Autowired
-    private SimpMessagingTemplate template;
+    private WebSocketService webSocketService;
 
-    public void sendNotification(UUID userId, String message) {
-        template.convertAndSendToUser(
-                userId.toString(), // Assuming userId is the destination user
-                "/queue/notifications", // Destination queue
-                message
-        );
+    public void sendRideRequestNotification(UUID driverId, Ride ride) {
+        try {
+            Map<String, Object> notification = new HashMap<>();
+            notification.put("rideId", ride.getId().toString());
+            notification.put("pickupLocation", ride.getPickupAddress());
+            notification.put("destination", ride.getDestinationAddress());
+            notification.put("price", ride.getPrice());
+
+            System.out.println("Sending notification to driver: " + driverId);
+            System.out.println("Message json " + new ObjectMapper().writeValueAsString(notification));
+
+            // The path should be: /user/{userId}/queue/ride-requests
+            webSocketService.sendMessageToUser(driverId.toString(), notification);
+        } catch (Exception e) {
+            System.err.println("Error sending notification: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
